e3a5a9ee8af914841bd4cd232a1f9d11f30fe1f5
Strix <660956+MrStrix@users.noreply.github.com>
2019-01-21 09:42:22 +0100

    split outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "kubernetes" {
-  value       = "${azurerm_kubernetes_cluster}"
+output "kubernetes_fqdn" {
+  value       = "${azurerm_kubernetes_cluster.default.fqdn}"
   description = "FQDN of kubernetes"
+}
+
+output "kubernetes_kubeconfig" {
+  value       = "${azurerm_kubernetes_cluster.default.kube_config_raw}"
+  description = "Kubeconfig"
 }
